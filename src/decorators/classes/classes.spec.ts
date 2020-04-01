b13b1f1ed161a4411692a5511121cf255384ec89
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-04-01 17:22:34 +0200

    Add test for Controller decorator

---
+++ src/decorators/classes/classes.spec.ts
@@ -0,0 +1,25 @@
+import {Controller} from './controller';
+import {Router} from 'express';
+import * as chai from 'chai';
+
+const expect = chai.expect;
+
+class TestSubject {
+
+    public router?: Router;
+
+}
+
+describe('Classes decorators', () => {
+
+    describe('Controller decorator', () => {
+
+        it('should add a router property to class', () => {
+            const NewType = Controller()(TestSubject);
+            const instance = new NewType();
+            expect(instance.router).not.to.be.undefined;
+        });
+
+    });
+
+});
