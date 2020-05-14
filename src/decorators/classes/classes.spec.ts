356b6c2454b592962d9e83b2de992600a2ec6ebc
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-05-14 14:12:43 +0200

    Fix tests

--- src/decorators/classes/classes.spec.ts
+++ src/decorators/classes/classes.spec.ts
@@ -15,7 +15,7 @@ describe('Classes decorators', () => {
     describe('Controller decorator', () => {
 
         it('should add a router property to class', () => {
-            const NewType = Controller()(TestSubject);
+            const NewType = Controller('/')(TestSubject);
             const instance = new NewType();
             expect(instance.router).not.to.be.undefined;
         });
